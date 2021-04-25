d01fd19dc1ff4c8f4241447bfbc514ab0956df0f
gabrielfranconascimento <gabrielfranconascimen@gmail.com>
2021-04-25 16:56:27 -0300

    Update config and create styles class

---
+++ reactNative/src/styles/colors.js
@@ -0,0 +1,14 @@
+const colors = { 
+    black: 'black',
+    white: 'white',
+    primary: '#6200EE',
+    primaryDark: '#3700B3',
+    accent: '#03DAC5',
+    primaryBlue: '#050533',
+    lightBlue: '#050566',
+    iceBeigeColor: '#F2F1E8',
+    gray: '#F0F0F0',
+    shadowBackground: '#50000000'
+}
+
+export default colors
